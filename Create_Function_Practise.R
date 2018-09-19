f92cdf6dfeaa399d451d94f07dd32c7d367a9e77
KirstiOcean <43409084+KirstiOcean@users.noreply.github.com>
2018-09-19 09:30:05 -0300

    Create_Function_Practice_Edit
    Edit and branching practice.

--- Create_Function_Practise.R
+++ Create_Function_Practise.R
@@ -1,3 +1,5 @@
 #Creating a function practise below
-invlogit = function(x){exp(x)/(exp(x)+1)}
+invlogit = function(x){exp(x)/(exp(x)+2)}
 # Inverse-logit link above
+
+#oops, should add 2 at the end of function insteasd of 1! (this is just editing practise)
